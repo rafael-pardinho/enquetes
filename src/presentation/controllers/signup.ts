6e57f6f8ff70343e8c282e4fb42cde2f446ba740
Rafael Pardinho <rafael.pardinho@rocketclouds.com.br>
2021-05-20 19:13:23 -0300

    feat: garantir que SignUpController retorne um erro se nenhum e-mail for fornecido

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,16 @@
 export class SignUpController {
-    handle (httpRequest: any): any {
-        return  {
-            statusCode: 400,
-            body: new Error('parâmetro name ausente')
+    handle(httpRequest: any): any {
+        if (!httpRequest.body.name) {
+            return {
+                statusCode: 400,
+                body: new Error('parâmetro name ausente')
+            }
+        }
+        if (!httpRequest.body.email) {
+            return {
+                statusCode: 400,
+                body: new Error('parâmetro email ausente')
+            }
         }
     }
 }
